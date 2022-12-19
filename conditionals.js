e906ebd35925c9a766955d90a175d517fc3475db
Allen Ding <allen.ding@hotmail.com>
2022-12-18 17:43:18 -0800

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,4 @@
-const raining = true;
+const raining = false;
 const cold = false;
 
 if(raining){
@@ -17,7 +17,7 @@ if(cold){
   console.log("Short sleeves are fine.");
 }
 
-const temperature = 12;
+const temperature = 41;
 
 if(temperature < 0){
   console.log("Make sure you pick out a scarf!");
@@ -28,3 +28,18 @@ if(temperature < 0){
 }
 
 console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if(isCitizen && age > 18){
+  console.log("You are eligible to vote.");
+}
+
+if(temperature < -40 || temperature > 40){
+  console.log("Maybe going outside isn't such a great idea..");
+}
+
+if(!raining){
+  console.log("Leave your umbrealla at home!");
+}
